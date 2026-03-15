c296f9868f3195d5c7465ae3600bb033cba63c02
jojoe77777 <jojoe77777@users.noreply.github.com>
2026-03-15 23:37:44 +1100

    added toolscreen downloader

--- build.bat
+++ build.bat
@@ -7,20 +7,25 @@ set "TEST_PRESET=release"
 set "RUN_TESTS=0"
 set "FAILURE_STEP="
 set "FAILURE_CODE=1"
+set "ARTIFACT_CONFIG_DIR=Release"
 
 for %%A in (%*) do (
     if /I "%%~A"=="release" (
         set "BUILD_PRESET=release"
         set "TEST_PRESET=release"
+        set "ARTIFACT_CONFIG_DIR=Release"
     ) else if /I "%%~A"=="debug" (
         set "BUILD_PRESET=debug"
         set "TEST_PRESET=debug"
+        set "ARTIFACT_CONFIG_DIR=Debug"
     ) else if /I "%%~A"=="--test" (
         set "RUN_TESTS=1"
     ) else (
         goto :usage
     )
 )
+
+set "ARTIFACT_DIR=%SCRIPT_DIR%out\build\bin\%ARTIFACT_CONFIG_DIR%"
 
 pushd "%SCRIPT_DIR%" >nul || exit /b 1
 
@@ -56,11 +61,26 @@ if errorlevel 1 (
     goto :fail
 )
 
+if exist "%ARTIFACT_DIR%\toolscreen-installer.jar" del /q "%ARTIFACT_DIR%\toolscreen-installer.jar"
+if exist "%ARTIFACT_DIR%\toolscreen-installer.exe" del /q "%ARTIFACT_DIR%\toolscreen-installer.exe"
+if exist "%ARTIFACT_DIR%\toolscreen-installer.pdb" del /q "%ARTIFACT_DIR%\toolscreen-installer.pdb"
+if exist "%ARTIFACT_DIR%\tioolscrteen-downloader.jar" del /q "%ARTIFACT_DIR%\tioolscrteen-downloader.jar"
+if exist "%ARTIFACT_DIR%\tioolscrteen-downloader.exe" del /q "%ARTIFACT_DIR%\tioolscrteen-downloader.exe"
+if exist "%ARTIFACT_DIR%\tioolscrteen-downloader.pdb" del /q "%ARTIFACT_DIR%\tioolscrteen-downloader.pdb"
+
 echo Building EXE package with preset %BUILD_PRESET%...
 cmake --build --preset %BUILD_PRESET% --target installer_exe
 if errorlevel 1 (
     set "FAILURE_STEP=Build installer EXE package"
     set "FAILURE_CODE=50"
+    goto :fail
+)
+
+echo Building Toolscreen downloader EXE with preset %BUILD_PRESET%...
+cmake --build --preset %BUILD_PRESET% --target downloader_exe
+if errorlevel 1 (
+    set "FAILURE_STEP=Build Toolscreen downloader EXE"
+    set "FAILURE_CODE=55"
     goto :fail
 )
 
